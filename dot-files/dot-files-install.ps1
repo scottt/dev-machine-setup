07d5105e189badb35875822ced25ea9255d1ac07
Scott Tsai <scottt@neuralplay.ai>
2026-03-04 19:08:40 +0800

    dot-files-install.ps1: install wezterm.lua

--- dot-files/dot-files-install.ps1
+++ dot-files/dot-files-install.ps1
@@ -2,3 +2,7 @@ PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
  
 New-Item $profile -Type File -Force
 cp Microsoft.PowerShell_profile.ps1 $profile
+
+# wezterm
+New-Item -ItemType Directory -Force -Path "$HOME\.config\wezterm" | Out-Null
+Copy-Item -Path ".\wezterm.lua" -Destination "$HOME\.config\wezterm\" -Force
